9e41e80e0c6fe6d532a5c586fd021c8a5d8c1272
ricci <ricci@DESKTOP-27DSUPC>
2019-03-10 18:51:47 -0300

    Correção tela produto

--- ButecoDoPiscaFX/src/db/botecodopiscafx/FXMLCADGarconController.java
+++ ButecoDoPiscaFX/src/db/botecodopiscafx/FXMLCADGarconController.java
@@ -1,11 +1,19 @@
 package db.botecodopiscafx;
 
 import com.jfoenix.controls.JFXTextField;
+import db.dal.DALProduto;
+import db.entidades.Produto;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextInputControl;
 
 public class FXMLCADGarconController implements Initializable 
 {
@@ -30,6 +38,34 @@ public class FXMLCADGarconController implements Initializable
     public void initialize(URL url, ResourceBundle rb) 
     {
         
+    }
+    
+    private void estadoOriginal() {
+        BtnPesquisar.setDisable(false);
+        BtnNovo.setDisable(true);
+        BtnConfirmar.setDisable(true);
+        BtnCancelar.setDisable(false);
+        BtnApagar.setDisable(true);
+        BtnAlterar.setDisable(true);
+        BtnNovo.setDisable(false);
+
+        ObservableList<Node> componentes = pnDados.getChildren(); //”limpa” os componentes
+        for (Node n : componentes) {
+            if (n instanceof TextInputControl) // textfield, textarea e htmleditor
+                ((TextInputControl) n).setText("");
+            if (n instanceof ComboBox)
+                ((ComboBox) n).getItems().clear();
+        }
+
+        carregaTabela("");
+    }
+
+    private void carregaTabela(String filtro) {
+        DALProduto dal = new DALProduto();
+        List<Produto> res = dal.get(filtro);
+        ObservableList<Produto> modelo;
+        modelo = FXCollections.observableArrayList(res);
+        tbvDados.setItems(modelo);
     }
 
     @FXML
